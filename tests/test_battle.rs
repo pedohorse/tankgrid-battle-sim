472747e1e8e00734c223a21e7ee900fb5b5e283d
pedohorse <13556996+pedohorse@users.noreply.github.com>
2024-12-15 13:03:48 +0100

    print str convert test, rand test

--- tests/test_battle.rs
+++ tests/test_battle.rs
@@ -1,16 +1,18 @@
 use battle_sim::battle::DEFAULT_COMMAND_DURATION;
 use battle_sim::map_object::MapObject;
 use battle_sim::object_layer::ObjectLayer;
-use battle_sim::r#impl::grid_battle::{GridBattle, new_player};
+use battle_sim::r#impl::grid_battle::{new_player, GridBattle};
 use battle_sim::r#impl::grid_map::GridBattleMap;
 use battle_sim::r#impl::grid_map_prober::GridMapProber;
 use battle_sim::r#impl::grid_orientation::GridOrientation;
-use battle_sim::r#impl::simple_battle_logic::{PlayerCommand, SimpleBattleLogic, MAX_LOG_LINE_LENGTH};
+use battle_sim::r#impl::simple_battle_logic::{
+    PlayerCommand, SimpleBattleLogic, MAX_LOG_LINE_LENGTH,
+};
 use battle_sim::r#impl::simple_battle_object_layer::SimpleBattleObjectLayer;
 use std::collections::HashMap;
 
 mod common;
-use common::{SimpleTileType, TestTrivialLogic, VecLogWriter, HashmapCommandTimer, FnCommandTimer};
+use common::{FnCommandTimer, HashmapCommandTimer, SimpleTileType, TestTrivialLogic, VecLogWriter};
 
 struct TestNoObjectCache {}
 
@@ -21,7 +23,9 @@ where
     fn new() -> Self {
         TestNoObjectCache {}
     }
-    fn add(&mut self, obj: MObj) -> u64 {0}
+    fn add(&mut self, obj: MObj) -> u64 {
+        0
+    }
     fn clear(&mut self) {}
     fn objects(&self) -> &[MObj] {
         &[]
@@ -52,11 +56,9 @@ fn testtest() {
             TestTrivialLogic {},
             GridMapProber {},
             SimpleBattleObjectLayer::new(),
-            FnCommandTimer::new(|com| {
-                match com {
-                    PlayerCommand::Print(_) => 0,
-                    _ => 10,
-                }
+            FnCommandTimer::new(|com| match com {
+                PlayerCommand::Print(_) => 0,
+                _ => 10,
             }),
             0,
         ),
@@ -88,13 +90,11 @@ fn test2players() {
             TestTrivialLogic {},
             GridMapProber {},
             SimpleBattleObjectLayer::new(),
-            FnCommandTimer::new(|com| {
-                match com {
-                    PlayerCommand::TurnCW => 10,
-                    PlayerCommand::MoveFwd => 20,
-                    PlayerCommand::Print(_) => 0,
-                    _ => 10,
-                }
+            FnCommandTimer::new(|com| match com {
+                PlayerCommand::TurnCW => 10,
+                PlayerCommand::MoveFwd => 20,
+                PlayerCommand::Print(_) => 0,
+                _ => 10,
             }),
             0,
         ),
@@ -146,11 +146,14 @@ fn test_2players_move_into_each_other() {
             TestTrivialLogic {},
             GridMapProber {},
             SimpleBattleObjectLayer::new(),
-            HashmapCommandTimer::new(HashMap::from([
-                (PlayerCommand::MoveFwd, 100),
-                (PlayerCommand::TurnCW, 10),
-                (PlayerCommand::MoveFwd, 20),
-            ]), 10),
+            HashmapCommandTimer::new(
+                HashMap::from([
+                    (PlayerCommand::MoveFwd, 100),
+                    (PlayerCommand::TurnCW, 10),
+                    (PlayerCommand::MoveFwd, 20),
+                ]),
+                10,
+            ),
             0,
         ),
         vec![
@@ -202,11 +205,14 @@ fn test_2players_move_past_each_other() {
             TestTrivialLogic {},
             GridMapProber {},
             SimpleBattleObjectLayer::new(),
-            HashmapCommandTimer::new(HashMap::from([
-                (PlayerCommand::MoveFwd, 100),
-                (PlayerCommand::TurnCW, 10),
-                (PlayerCommand::MoveFwd, 20),
-            ]), 10),
+            HashmapCommandTimer::new(
+                HashMap::from([
+                    (PlayerCommand::MoveFwd, 100),
+                    (PlayerCommand::TurnCW, 10),
+                    (PlayerCommand::MoveFwd, 20),
+                ]),
+                10,
+            ),
             0,
         ),
         vec![
@@ -258,14 +264,12 @@ fn test_2players_move_into_each_other_but_shoot() {
             TestTrivialLogic {},
             GridMapProber {},
             SimpleBattleObjectLayer::new(),
-            FnCommandTimer::new(|com| {
-                match com {
-                    PlayerCommand::TurnCW => 10,
-                    PlayerCommand::MoveFwd => 20,
-                    PlayerCommand::Shoot => 5,
-                    PlayerCommand::Print(_) => 0,
-                    _ => 10,
-                }
+            FnCommandTimer::new(|com| match com {
+                PlayerCommand::TurnCW => 10,
+                PlayerCommand::MoveFwd => 20,
+                PlayerCommand::Shoot => 5,
+                PlayerCommand::Print(_) => 0,
+                _ => 10,
             }),
             0,
         ),
@@ -306,7 +310,7 @@ fn test_2players_move_into_each_other_but_shoot() {
         b.player_state(1).col,
         b.player_state(1).row
     );
-    assert_eq!(45 + 30, b.time());  // 30 - is 3 waits for default 10 each
+    assert_eq!(45 + 30, b.time()); // 30 - is 3 waits for default 10 each
     assert_eq!(GridOrientation::East, b.player_state(0).orientation);
     assert_eq!(GridOrientation::West, b.player_state(1).orientation);
     assert_eq!(0, b.player_state(0).col);
@@ -327,13 +331,11 @@ fn test2players_inf_loop() {
             TestTrivialLogic {},
             GridMapProber {},
             SimpleBattleObjectLayer::new(),
-            FnCommandTimer::new(|com| {
-                match com {
-                    PlayerCommand::TurnCW => 10,
-                    PlayerCommand::MoveFwd => 20,
-                    PlayerCommand::Print(_) => 0,
-                    _ => 10,
-                }
+            FnCommandTimer::new(|com| match com {
+                PlayerCommand::TurnCW => 10,
+                PlayerCommand::MoveFwd => 20,
+                PlayerCommand::Print(_) => 0,
+                _ => 10,
             }),
             0,
         ),
@@ -377,13 +379,11 @@ fn test2players_bad_inf_loop() {
             TestTrivialLogic {},
             GridMapProber {},
             SimpleBattleObjectLayer::new(),
-            FnCommandTimer::new(|com| {
-                match com {
-                    PlayerCommand::TurnCW => 10,
-                    PlayerCommand::MoveFwd => 20,
-                    PlayerCommand::Print(_) => 0,
-                    _ => 10,
-                }
+            FnCommandTimer::new(|com| match com {
+                PlayerCommand::TurnCW => 10,
+                PlayerCommand::MoveFwd => 20,
+                PlayerCommand::Print(_) => 0,
+                _ => 10,
             }),
             0,
         ),
@@ -465,12 +465,118 @@ fn test_print_limit() {
     b.run_simulation();
     println!("BATTLE LOG:");
     b.log_writer().print();
-    assert_eq!(10 + 4*7 + 4*7, b.time());
+    assert_eq!(10 + 4 * 7 + 4 * 7, b.time());
     assert_eq!(GridOrientation::West, b.player_state(0).orientation);
 
     assert_eq!("log[line1]", &b.log_writer().log_datas[2].1);
-    assert_eq!("log[line3 _next line should not be allowed__foo!]", &b.log_writer().log_datas[8].1);
-    assert_eq!(MAX_LOG_LINE_LENGTH + "log[]".len(), b.log_writer().log_datas[5].1.len());  // check truncation
+    assert_eq!(
+        "log[line3 _next line should not be allowed__foo!]",
+        &b.log_writer().log_datas[8].1
+    );
+    assert_eq!(
+        MAX_LOG_LINE_LENGTH + "log[]".len(),
+        b.log_writer().log_datas[5].1.len()
+    ); // check truncation
     assert_eq!("log[---next print will be muted and penalized with game time unless a valid game comand called---]", &b.log_writer().log_datas[17].1);
     assert_eq!(60, b.log_writer().log_datas.len());
+}
+
+#[test]
+fn test_print_convert() {
+    let map = GridBattleMap::new(2, 2, SimpleTileType::Nothin, SimpleTileType::Nothin);
+    let logger = VecLogWriter::new();
+    let mut b = GridBattle::new(
+        SimpleBattleLogic::new(
+            map,
+            TestTrivialLogic {},
+            GridMapProber {},
+            SimpleBattleObjectLayer::new(),
+            FnCommandTimer::new(|com| match com {
+                PlayerCommand::Print(_) => 0,
+                _ => 10,
+            }),
+            0,
+        ),
+        vec![(
+            new_player(0, 0, GridOrientation::South, 0, 1, "player1"),
+            "\
+            print('foo', 1, 3.3)\n\
+            "
+            .to_owned(),
+        )],
+        logger,
+    );
+    b.run_simulation();
+    println!("BATTLE LOG:");
+    b.log_writer().print();
+
+    assert_eq!(0, b.time());
+    assert_eq!("log[foo 1 3.3]", &b.log_writer().log_datas[2].1);
+}
+
+#[test]
+fn test_rand() {
+    // run 2 sims - rand results must be identical
+    let mut bs = Vec::with_capacity(3);
+    for i in 0..3 {
+        let map = GridBattleMap::new(2, 2, SimpleTileType::Nothin, SimpleTileType::Nothin);
+        let logger = VecLogWriter::new();
+        let mut b = GridBattle::new(
+            SimpleBattleLogic::new(
+                map,
+                TestTrivialLogic {},
+                GridMapProber {},
+                SimpleBattleObjectLayer::new(),
+                FnCommandTimer::new(|com| match com {
+                    PlayerCommand::Print(_) => 0,
+                    _ => 10,
+                }),
+                0,
+            ),
+            vec![(
+                new_player(0, 0, GridOrientation::South, 0, 1, "player1"),
+                "\
+                r1 = rand()\n\
+                r2 = rand()\n\
+                r3 = rand()\n\
+                r4 = rand()\n\
+                r5 = rand()\n\
+                print(r1, r2, r3, r4, r5)\n\
+                "
+                .to_owned()
+                    + if i == 2 { "pass\n" } else { "" },  // program for 3rd sim has different text - so should do different rand
+            )],
+            logger,
+        );
+        bs.push(b);
+    }
+    bs[0].run_simulation();
+    bs[1].run_simulation();
+    bs[2].run_simulation();
+    println!("BATTLE LOG1:");
+    bs[0].log_writer().print();
+    println!("BATTLE LOG2:");
+    bs[1].log_writer().print();
+    println!("BATTLE LOG3:");
+    bs[2].log_writer().print();
+
+    let mut valss = Vec::with_capacity(3);
+    for b in bs {
+        let line_len = b.log_writer().log_datas[2].1.len();
+        let vals: Vec<f64> = b.log_writer().log_datas[2].1[4..line_len - 1]
+            .split(' ')
+            .map(|x| -> f64 { x.parse().unwrap() })
+            .collect();
+        // assert that all valuse are different
+        for i in 1..vals.len() {
+            assert_ne!(vals[i], vals[i - 1]);
+        }
+        assert!(vals.iter().fold(999_f64, |a, b| a.min(*b)) >= 0_f64);
+        assert!(vals.iter().fold(-999_f64, |a, b| a.max(*b)) < 1_f64);
+        valss.push(vals);
+    }
+    // assert that sims with same parameters yield same rand results
+    assert!(valss[0].iter().zip(valss[1].iter()).all(|(a, b)| *a == *b));
+    // assert  sims with different programs yield different rand results
+    assert!(valss[1].iter().zip(valss[2].iter()).all(|(a, b)| *a != *b));
 }
